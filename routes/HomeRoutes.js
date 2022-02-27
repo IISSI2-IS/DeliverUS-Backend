3bc8cadb4d80f34d5e134d9725d953fbe8187c71
alejandrofdez-us <afdez@us.es>
2022-02-27 08:56:26 +0100

    Fixed home response so it forwards users to the correct url

--- routes/HomeRoutes.js
+++ routes/HomeRoutes.js
@@ -3,6 +3,6 @@
 module.exports = (options) => {
   const app = options.app
   app.get('/', (req, res) => {
-    res.send('Deliverus API. Check <a href="https://github.eii.us.es/IISSI2-IS/IISSI2-IS-Backend/wiki">Repository Wiki</a>')
+    res.send('Deliverus API. Check <a href="https://github.com/IISSI2-IS/DeliverUS-Backend">Repository</a>')
   })
 }
