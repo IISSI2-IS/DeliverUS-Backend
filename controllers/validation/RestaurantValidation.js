4744d92a19486efe057f935180c9381bd3b45a4f
alejandrofdez-us <afdez@us.es>
2022-03-12 08:34:35 +0100

    resolves #8

--- controllers/validation/RestaurantValidation.js
+++ controllers/validation/RestaurantValidation.js
@@ -51,7 +51,8 @@ module.exports = {
         .custom((value, { req }) => {
           return FileValidationHelper.checkFileMaxSize(req.files.logo[0], maxFileSize)
         })
-        .withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
+        .withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+      check('email').isEmail()
     ]
   }
 }
